557c89dab97ca09c5f830d0dbb2ee663f490d699
SK-4 <sohamk2002@gmail.com>
2023-05-10 16:12:26 +0530

    modifying the UI

--- Healthcare/urls.py
+++ Healthcare/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('account/',include('accounts.urls')),
+    path('accounts/',include('accounts.urls')),
     path('appoinment/',include('appoinment.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
