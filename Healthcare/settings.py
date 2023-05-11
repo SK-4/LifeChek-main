2cf156e9ec5888e618918b8cdc3c59bd38a1c841
SK-4 <sohamk2002@gmail.com>
2023-05-11 10:38:35 +0530

    major ui changes

--- Healthcare/settings.py
+++ Healthcare/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS= ( os.path.join(BASE_DIR, 'home/assets'),)
+STATICFILES_DIRS= ( os.path.join(BASE_DIR, 'assets'),)
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('patient/register', RegisterPatientView.as_view(), name='patient-register'),
-    path('login', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('doctor/register', RegisterDoctorView.as_view(), name='doctor-register'),
     path('patient/profile/update/', EditPatientProfileView.as_view(), name='patient-profile-update'),
